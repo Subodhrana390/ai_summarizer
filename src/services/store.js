f0c470dfa295a086734a6a7009ea6e8e100861d9
subodhrana390 <subodhrana390@gmail.com>
2024-01-22 09:20:25 +0530

    second

--- src/services/store.js
+++ src/services/store.js
@@ -1,5 +1,5 @@
 import { configureStore } from "@reduxjs/toolkit";
-import { articleApi } from "./article";
+import { articleApi } from "./article.js";
 
 export const store = configureStore({
   reducer: {
